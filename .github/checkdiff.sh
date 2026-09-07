afba6053b16cfc887b13d86399c37384ff0f6bf6
spacky1 <58446743+spacky1@users.noreply.github.com>
2026-09-07 14:38:28 -0700

    remove unnecessary files

--- .github/checkdiff.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-if ! git diff-index --quiet HEAD --; then
-    echo 'Uncommitted changes detected:'
-    git diff-index HEAD --
-    return 1
-fi
--- rgbdscheck.asm
+++
@@ -1,6 +0,0 @@
-IF !DEF(__RGBDS_MAJOR__)
-	fail "pokecrystal requires rgbds v1.0.0 or newer."
-ENDC
-IF __RGBDS_MAJOR__ < 1
-	fail "pokecrystal requires rgbds v1.0.0 or newer."
-ENDC
